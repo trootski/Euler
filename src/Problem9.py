70ae52e9bf2f13a6bf3c598d583e649dc53f6703
John Byrne <troot1@gmail.com>
2013-09-14 16:36:16 +0100

    Started working on solution

--- src/Problem9.py
+++ src/Problem9.py
@@ -9,7 +9,15 @@ def testForWin(a, b, c):
         if (math.pow(a, 2) + math.pow(b, 2)) == math.pow(c, 2):
             return True
 
-def testCVal(c):
+def testPyThag(a, b, c):
+    if (math.pow(a, 2) + math.pow(b, 2)) == math.pow(c, 2):
+        return True
+
+for test_a in range(a_val, 999):
+	if testPyThag(a_val, b_val, c_val):
+		if testForWin(a_val, b_val, c_val):
+			print "Answer is: %s + %s + %s" % (a_val, b_val, c_val)
+
 
 for test_c in range(c_val, 999):
 
