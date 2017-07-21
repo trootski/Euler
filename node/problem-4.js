53d3ada86df9d8ccdf7da433ceecf189c935cb54
John Byrne <troot1@gmail.com>
2017-07-21 22:04:24 +0100

    Refactor isPalindrome solution

--- node/problem-4.js
+++ node/problem-4.js
@@ -4,11 +4,18 @@
 
 function isPalindrome(num) {
   const arrMid = Math.floor(num.toString().length);
+  const mirrorVal = (arr, i) => arr[arr.length - i - 1];
   return num
     .toString()
     .split('')
-    .find((ele, i, arr) => { console.log(ele, arr[arr.length - i - 1]); return i < arrMid && ele !== arr[arr.length - i - 1]; })
+    .find((ele, i, arr) => {
+      if (i >= arrMid) {
+        return true;
+      } else {
+        return ele !== mirrorVal(arr, i);
+      }
+    }) === undefined
 }
 
-console.log(isPalindrome(9009));
+console.log(isPalindrome(19091));
 
