663f7b5913d00bb8b40769f15e6281ddfd3e1ae7
John Byrne <troot1@gmail.com>
2018-10-10 22:35:34 +0100

    wip: update intellij layout

--- scala/src/problem_2.scala
+++ scala/src/problem_2.scala
@@ -1,4 +1,4 @@
-object problem_1 {
+object problem_2 {
   def main(args:Array[String]) {
     def isDivisibleBy = (v: Int) => ((v % 3) == 0 || (v % 5) == 0)
     println((1 to 999).filter(isDivisibleBy).reduceLeft(_+_))
